3b32de7d69b39edeba5df2ba1cd443eef7cc077a
Javlonbek <javlonrahmatullayev07@gmail.com>
2024-08-27 12:18:54 +0500

    Create Conclusion Model and Migration

---
+++ app/Models/Conclusion.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Conclusion extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'number_out',
+        'number_in',
+        'date_out',
+        'date_in',
+        'file',
+        'act_id'
+    ];
+}
