66f7eabd6328aac335e146735ae8c9b73f6f3d65
Javlonbek <javlonrahmatullayev07@gmail.com>
2024-08-27 12:32:56 +0500

    Create Resolution Model and Migration

---
+++ database/migrations/2024_08_27_073023_create_resolutions_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('resolutions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('parent_id');
+            $table->string('resolution_number');
+            $table->date('resolution_date');
+            $table->foreignId('conclusion_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('resolutions');
+    }
+};
