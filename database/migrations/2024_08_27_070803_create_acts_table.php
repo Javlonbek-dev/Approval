2538aa68c61936e4aa00bb773c1f47a1fac7c59f
Javlonbek <javlonrahmatullayev07@gmail.com>
2024-08-27 12:11:12 +0500

    Create Act Model and Migration

---
+++ database/migrations/2024_08_27_070803_create_acts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('acts', function (Blueprint $table) {
+            $table->id();
+            $table->string('type');
+            $table->integer('parent_id');
+            $table->date('act_date');
+            $table->string('act_number');
+            $table->foreignId('order_id')->constrained('orders');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('acts');
+    }
+};
