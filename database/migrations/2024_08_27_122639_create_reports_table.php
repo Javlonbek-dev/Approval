1530ab9534d8abe733e8d7fc49fe498d3149833e
Javlonbek <javlonrahmatullayev07@gmail.com>
2024-08-28 14:39:55 +0500

    Code rub

--- database/migrations/2024_08_27_122639_create_reports_table.php
+++ database/migrations/2024_08_27_122639_create_reports_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -17,7 +16,6 @@ return new class extends Migration
             $table->string('number_in');
             $table->date('date_out');
             $table->date('date_in');
-            $table->string('report_file');
             $table->foreignId('act_id')->constrained('act')->onDelete('cascade');
             $table->foreignId('executor_id')->constrained('executors')->onDelete('cascade');
             $table->timestamps();
