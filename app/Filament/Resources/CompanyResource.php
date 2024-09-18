91e4247b1731bc13be5c9cd9515c67acee44afd5
Javlonbek <javlonrahmatullayev07@gmail.com>
2024-09-18 10:08:37 +0500

    Add Company Filament

---
+++ app/Filament/Resources/CompanyResource.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CompanyResource\Pages;
+use App\Filament\Resources\CompanyResource\RelationManagers;
+use App\Models\Company;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class CompanyResource extends Resource
+{
+    protected static ?string $model = Company::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required(),
+                Forms\Components\TextInput::make('stir')
+                    ->required()
+                    ->unique(),
+                Forms\Components\TextInput::make('phone')
+                    ->tel()
+                    ->required(),
+                Forms\Components\TextInput::make('email')
+                    ->email(),
+                Forms\Components\TextInput::make('address')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('website')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('manager')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('bank_visits')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Select::make('region_id')
+                    ->relationship('region', 'name')
+                    ->required(),
+                Forms\Components\Select::make('thsht_id')
+                    ->relationship('thsht', 'name_extend')
+                    ->required(),
+                Forms\Components\Select::make('ifut_id')
+                    ->relationship('ifut', 'name_extend')
+                    ->required(),
+                Forms\Components\Select::make('dbit_id')
+                    ->relationship('dbit', 'name_extend')
+                    ->required(),
+            ])->columns(1);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('stir')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('phone')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('email')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('address')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('website')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('manager')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('bank_visits')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('region.name')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('thsht.name_extend')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('ifut.name_extend')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('dbit.name_extend')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('deleted_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCompanies::route('/'),
+            'create' => Pages\CreateCompany::route('/create'),
+            'edit' => Pages\EditCompany::route('/{record}/edit'),
+        ];
+    }
+}
