820e8ba72747ec1b3707ab62f82329e51a83d30e
Javlonbek <javlonrahmatullayev07@gmail.com>
2024-09-23 12:26:44 +0500

    Create Approval Resources by filament

---
+++ app/Filament/Resources/ApprovalResource/Pages/EditApproval.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\ApprovalResource\Pages;
+
+use App\Filament\Resources\ApprovalResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditApproval extends EditRecord
+{
+    protected static string $resource = ApprovalResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
---
+++ app/Filament/Resources/ApprovalResource/Pages/ListApprovals.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\ApprovalResource\Pages;
+
+use App\Filament\Resources\ApprovalResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListApprovals extends ListRecords
+{
+    protected static string $resource = ApprovalResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+}
